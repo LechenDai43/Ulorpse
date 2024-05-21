fddfe54adaf12d916cb51c310b49381eef09d3de
LechenDai43 <46640649+LechenDai43@users.noreply.github.com>
2024-05-20 21:31:10 -0400

    refine the introduction scene

--- Assets/Codes/IntroductionScene/IntroductionManagement.cs
+++ Assets/Codes/IntroductionScene/IntroductionManagement.cs
@@ -9,10 +9,14 @@ public class IntroductionManagement : MonoBehaviour
 {
     public TextAsset IntroductionLineFileCN, IntroductionLineFileEN, IntroductionLineFileJP;
     public Dictionary<string, string> IntroductionLines;
+    public TextAsset LineVariable;
+    public List<string> LineVariableList = new List<string>();
+    public GameObject FirstPane, SecondPane;
 
     public Text[] LinesText = new Text[11];
     private int linePointer = 0;
     private bool dialogueFinished = false;
+    private bool dialogueSkipped = false;
 
     // Start is called before the first frame update
     void Start()
@@ -35,6 +39,12 @@ public class IntroductionManagement : MonoBehaviour
             line.gameObject.SetActive(false);
         }
 
+        string lineVariableContent = LineVariable.text;
+        foreach (string line in lineVariableContent.Split('\n'))
+        {
+            LineVariableList.Add(line.Trim('\r'));
+        }
+
         StartCoroutine(ShowLines());
     }
 
@@ -42,26 +52,43 @@ public class IntroductionManagement : MonoBehaviour
     {
         foreach (Text line in LinesText)
         {
-            linePointer += 1;
-            string variableName = "intro";
-            if (linePointer < 10)
-            {
-                variableName += 0.ToString() + linePointer.ToString();
-            }
-            else
-            {
-                variableName += linePointer.ToString();
-            }
+            string variableName = LineVariableList[linePointer];
             line.text = IntroductionLines[variableName];
             line.gameObject.SetActive(true);
 
-            if(linePointer == 11)
+            
+            linePointer += 1;
+            if(linePointer == 10)
             {
                 dialogueFinished = true;
             }
 
-            yield return new WaitForSeconds(3);
+            if (dialogueSkipped) {   
+                yield break;
+            }
+            yield return new WaitForSeconds(1);
         }
+    }
+
+    public void SkipDialogue() {
+        if (dialogueFinished) {
+            FirstPane.SetActive(false);
+            SecondPane.SetActive(true);
+        }
+        else {            
+            dialogueSkipped = true;
+            _finishAllLines();
+        }
+    }
+
+    private void _finishAllLines() {
+        for (int i = linePointer; i < LinesText.Length; i ++) {
+            string variableName = LineVariableList[i];
+            Text line = LinesText[i];
+            line.text = IntroductionLines[variableName];
+            line.gameObject.SetActive(true);
+        }
+        dialogueFinished = true;
     }
 
     // Update is called once per frame
