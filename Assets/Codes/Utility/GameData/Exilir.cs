aba1cdb1894b2861ccbcab03364872cbaf55b8c8
LechenDai43 <46640649+LechenDai43@users.noreply.github.com>
2024-05-31 14:22:37 -0400

    organize game data

---
+++ Assets/Codes/Utility/GameData/Exilir.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exilir
+{
+
+}
+
+public enum Elements {
+	Mental = 1,
+	Wood = 2,
+	Water = 3,
+	Fire = 4,
+	Earth = 5,
+	Normal = 6
+}
